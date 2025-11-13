34d3f9ff958961f94431faa050eb6f2863a21ba8
Alissar Pirugini <alissarsilva@gmail.com>
2025-11-13 16:20:25 -0300

    Add files via upload

---
+++ calculadora.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Calculadora simples"
+echo "Digite o primeiro número:"
+read num1
+echo "Digite o segundo número:"
+read num2
+echo "Escolha a operação (+ - * /):"
+read op
+
+case $op in
+    +) resultado=$((num1 + num2)) ;;
+    -) resultado=$((num1 - num2)) ;;
+    \*) resultado=$((num1 * num2)) ;;
+    /) resultado=$((num1 / num2)) ;;
+    *) echo "Operação inválida!" ; exit 1 ;;
+esac
+
+echo "Resultado: $resultado"
+
+
